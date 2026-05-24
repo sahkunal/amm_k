5e6581d81f764c002b846fe648e7161e6b72161e
sahkunal <kunalcode01@gmail.com>
2026-05-24 13:38:07 +0000

    First commit

---
+++ programs/amm_k/src/lib.rs
@@ -0,0 +1,34 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("DU55WLSWzpCP55TYP96zU6gq5PRRHqcYKK1mkhsVj6jV");
+
+#[program]
+pub mod amm_k {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>,
+    seed : u64,
+    fee: u16,
+    authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed,fee,authority,ctx.bumps)
+    }
+    pub fn deposit(ctx:Context<Deposit>, amount: u64, max_x:u64, max_y:u64)-> Result<()>{
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+    pub fn withdraw(ctx:Context<Withdraw>, amount: u64, min_x:u64, min_y:u64)-> Result<()>{
+        ctx.accounts.withdraw(amount, min_x, min_y)
+    }
+    pub fn swap(ctx:Context<Swap>, is_x: bool, amount: u64, min: u64)-> Result<()>{
+        ctx.accounts.swap(is_x,amount, min)
+    }
+}
